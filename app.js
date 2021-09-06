0e76c1cf015cc1c1f5b0a1e0af15577a6bda7a67
Xuphur <40891934+Xuphur@users.noreply.github.com>
2021-09-07 01:13:00 +0500

    mongodb connection update

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+});
 
 // connect Mongo Data base via Mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ams', { useNewUrlParser: true });
-// mongoose.connect('mongodb://admin:admin123@ds147125.mlab.com:47125/ams', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/ams', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:2TAOnsZZP5z1ygyO@cluster0.t5ien.mongodb.net/ams', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('mongo is connected');
@@ -44,18 +44,18 @@ require('./routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(4000, () => console.log('server is running on port 4000'));
