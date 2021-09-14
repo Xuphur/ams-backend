54fc73e2a505ad13267ab03fef83eb9a2bc0df08
Xuphur <40891934+Xuphur@users.noreply.github.com>
2021-09-14 20:26:17 +0500

    LTS-1.0.1

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 'use strict';
 
-module.exports = function(app){
+module.exports = function(app) {
     // app.use('/veh', require('./api/vehicles'));
     // app.use('/user', require('./api/users'));
-    // app.use('/contract', require('./api/contract'));
+    app.use('/contract', require('./api/contract'));
     app.use('/customer', require('./api/customer'));
     app.use('/asset', require('./api/asset'));
     app.use('/reciept', require('./api/reciept'));
